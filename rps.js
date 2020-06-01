cfea2807021c81276b13a48446e38a8cc011fa6d
zieeco <zieecosam@gmail.com>
2020-06-01 05:14:21 +0100

    corrected js error

--- rps.js
+++ rps.js
@@ -4,8 +4,10 @@ const weaponButtons = document.querySelectorAll('button');
 const rockButton = document.querySelector('.rockButton');
 const paperButton = document.querySelector('.paperButton');
 const scissorsButton = document.querySelector('.scissorsButton');
-
+const updateComputer = document.querySelector('#computer-score p');
+const updatePlayer = document.querySelector('#player-score p');
 const reset = document.getElementById('reset');
+const winner = document.querySelector('.instruction-text');
 
 rockButton.addEventListener('click', ()=>{
   playRound('rock', computerPlay());
@@ -18,12 +20,12 @@ paperButton.addEventListener('click', ()=>{
 scissorsButton.addEventListener('click', ()=>{
   playRound('scissors', computerPlay());
 });
-
-reset.onclick = () => {
-  updateComputer.textContent = computer;
-  updatePlayer.textContent = player;
+  
+ reset.onclick = () => {
+  winner.textContent = 'choose one of this options to play!';
+  updateComputer.textContent = 0;
+  updatePlayer.textContent = 0;
 };
-reset();
 
 function game(){ 
   weaponButtons.forEach((button) => {
@@ -36,18 +38,18 @@ function increaseScore(){
   const updatePlayer = document.querySelector('#player-score p');
   updateComputer.textContent = computer;
   updatePlayer.textContent = player;
+  
 }
 
 function computerPlay(){
   let computerSelection = Math.floor(Math.random()*3) +1;
-    if (computerSelection === 1 ) {
+    if (computerSelection === 1 ) 
       return computerSelection = 'rock';
-    }else if ( computerSelection === 2) {
+    else if ( computerSelection === 2) 
       return computerSelection = 'paper';
-    } else { 
+     else if (computerSelection === 3)  
       return computerSelection = 'scissors';
   }
-}
 
 function playRound(playerSelection, computerSelection) {
   
@@ -57,14 +59,20 @@ if ((playerSelection) === (computerSelection)) {
      winner.textContent = 'Draw!';
      return;
   }
-  else if ((playerSelection === 'rock') && (computerSelection === 'paper')){
-    winner.textContent = 'Computer wins! Paper beats rock.';
+  else if ((playerSelection === 'rock') && (computerSelection === 'scissors')){
+    winner.textContent = 'You win! Rock beats scissors.';
+    player++;
+    increaseScore();
+    return;
+  }
+  else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {  
+    winner.textContent = 'Computer wins! scissors beats paper.';
     computer++;
     increaseScore();
     return;
   }
-  else if ((playerSelection === 'rock') && (computerSelection === 'scisors')) {  
-    winner.textContent = 'You win! Rock beats scissors.';
+  else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+    winner.textContent = 'You win! scissors beats paper.';
     player++;
     increaseScore();
     return;
@@ -75,23 +83,17 @@ if ((playerSelection) === (computerSelection)) {
     increaseScore();
     return;
   }
-  else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
-    winner.textContent = 'You win! Paper beats rock.';
-    player++;
-    increaseScore();
-    return;
-  }
-  else if ((playerSelection === 'scissors') && (computerSelection === 'rock')) {
-    winner.textContent = 'Computer wins! Rock beats scissors.';
-    computer++;
-    increaseScore();
-    return;
-  }
-  else if ((playerSelection === 'scissors ') && (computerSelection === 'paper')) {
+  else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
     winner.textContent = 'You win! Scissors beats paper.';
     player++;
     increaseScore();
     return;
+  }
+  else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
+    winner.textContent = 'Computer wins! Paper beats rock.';
+    computer++;
+    increaseScore();
+    return;
  }
+ game();
 }
-game();
